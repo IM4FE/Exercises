26469047e4c24c50496b98abf840af483e59df61
Selivanov Dmitrii <dima.selivanov1020@mail.ru>
2023-08-17 20:00:56 +0300

    Добавьте файлы проекта.

---
+++ test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static string Mirror(int strLength, string str)
+        {
+            string res="";
+            for (int i = 0; i < strLength; i++)
+            {
+                res += str.Substring(strLength - i-1, 1);
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string strRes = "";
+            if (str.Length%2==0)
+            {
+                int strLength = str.Length / 2;
+                string strTemp1=str.Substring(0,strLength);
+                string strTemp2 = str.Substring(strLength);
+                strRes+=Mirror(strLength, strTemp1);
+                strRes+=Mirror(strLength, strTemp2);
+            }
+            else
+            {
+                strRes = Mirror(str.Length, str) + str;
+            }
+            Console.WriteLine(strRes);
+            Console.ReadLine();
+        }
+    }
+}
