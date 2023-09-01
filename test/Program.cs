06db6f2b040aa0afd4922ef6337b6c4f0beae847
Selivanov Dmitrii <dima.selivanov1020@mail.ru>
2023-09-01 14:33:20 +0300

    Задание 3

--- test/Program.cs
+++ test/Program.cs
@@ -9,12 +9,39 @@ namespace test
 {
     internal class Program
     {
+        static string CountNumLetter (string str)
+        {
+            char[] lettersAll = str.ToCharArray ();
+            List<char> lettersSort=new List<char>();
+            for (int i= 0; i < str.Length; i++)
+            {
+                if (lettersSort.Contains(str[i]))
+                {
+                    continue;
+                }
+                else
+                    lettersSort.Add(str[i]);
+            }
+            for (int i= 0; i < lettersSort.Count; i++)
+            {
+                int num = 0;
+                for (int j=0; j< lettersAll.Length;j++)
+                {
+                    if (lettersSort[i]==lettersAll[j])
+                    {
+                        num++;
+                    }    
+                }
+                str += "\nСимвол '" + lettersSort[i] + "' повторяется " + num + " раз";
+            }
+            return str;
+        }
         static string Mirror(int strLength, string str)
         {
-            string res="";
+            string res = "";
             for (int i = 0; i < strLength; i++)
             {
-                res += str.Substring(strLength - i-1, 1);
+                res += str.Substring(strLength - i - 1, 1);
             }
             return res;
         }
@@ -22,17 +49,46 @@ namespace test
         {
             string str = Console.ReadLine();
             string strRes = "";
-            if (str.Length%2==0)
+            if (!(str.Count(Char.IsUpper) > 0))
             {
-                int strLength = str.Length / 2;
-                string strTemp1=str.Substring(0,strLength);
-                string strTemp2 = str.Substring(strLength);
-                strRes+=Mirror(strLength, strTemp1);
-                strRes+=Mirror(strLength, strTemp2);
+                if (str.Length % 2 == 0)
+                {
+                    int strLength = str.Length / 2;
+                    string strTemp1 = str.Substring(0, strLength);
+                    string strTemp2 = str.Substring(strLength);
+                    strRes += Mirror(strLength, strTemp1);
+                    strRes += Mirror(strLength, strTemp2);
+                }
+                else
+                {
+                    strRes = Mirror(str.Length, str) + str;
+                }
+                strRes = CountNumLetter(strRes);
             }
             else
             {
-                strRes = Mirror(str.Length, str) + str;
+                strRes = "Ошибка. Были введены не подходящие символы! Неприемлемые символы:";
+                List<char> lettersUp = new List<char>();
+                for (int i = 0; i < str.Length; i++)
+                {
+                    char letter = char.Parse(str.Substring(i, 1));
+                    if (Char.IsUpper(letter))
+                    {
+                        if (lettersUp.Contains(str[i]))
+                        {
+                            continue;
+                        }
+                        else
+                            lettersUp.Add(str[i]);
+                    }
+                }
+                for (int i = 0; i < lettersUp.Count; i++)
+                {
+                    if (i == 0)
+                            strRes += lettersUp[i];
+                        else
+                            strRes += "," + lettersUp[i];
+                }
             }
             Console.WriteLine(strRes);
             Console.ReadLine();
