5e6abaa735fb63e9b4cbf84db7cc3671882f0763
Selivanov Dmitrii <dima.selivanov1020@mail.ru>
2023-09-28 17:11:10 +0300

    Задание 6. Вывод "урезанной" строки. Работа с Random.org API (POST запрос).

---
+++ test/ModelsRandApi/Response/LowerBaseResonce.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.ModelsRandApi.Response
+{
+    internal class LowerBaseResonce <T>
+    {
+        public LowestBaseResponce<T> random { get; set; }
+    }
+}
